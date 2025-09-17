0c2ceecb7d9db7bf553a0bceff8097125bcbe86c
Kubilay <kubilaysabah@gmail.com>
2025-09-17 12:51:12 +0300

    update register api

--- app/api/(auth)/register/route.ts
+++ app/api/(auth)/register/route.ts
@@ -1,24 +1,40 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@/lib/generated/client";
+import bcrypt from "bcryptjs";
 
 export async function POST(request: Request) {
-  const body = await request.body;
-  console.log("request", request.body);
-  console.log("awaited request", body);
-//   const prisma = new PrismaClient();
+  const body = await request.json();
 
-//   prisma.user.create({
-//     data: {
-//       firstName: body.firstName,
-//       lastName: "",
-//       birthDate: "",
-//       gender: "",
-//       phone: "",
-//       turkish_identity_number: "",
-//       password: "",
-//       email: "",
-//     },
-//   });
+  const prisma = new PrismaClient();
 
-  return NextResponse.json({ success: true }, { status: 200 })
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        firstName: body.firstName,
+        lastName: body.lastName,
+        birthDate: body.birthDate,
+        gender: body.gender,
+        phone: body.phone,
+        turkish_identity_number: body.turkish_identity_number,
+        password: hashedPassword,
+        email: body.email,
+      },
+      select: {
+        firstName: true,
+        lastName: true,
+        phone: true,
+        email: true,
+        gender: true,
+        birthDate: true,
+        turkish_identity_number: true,
+        image: true,
+      }
+    })
+
+    return NextResponse.json({ success: true, data: user, }, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ success: false, data: null }, { status: 500 });
+  }
 }
